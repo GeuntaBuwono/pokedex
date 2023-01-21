cc4dd970e9091f47efc26ab2e631d0c32c356dbe
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2023-01-21 11:00:11 +0700

    chore: add path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,32 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [['@babel/plugin-proposal-private-methods', {loose: true}]],
+  plugins: [
+    ['@babel/plugin-proposal-private-methods', {loose: true}],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          'components/*': ['components/*'],
+          'constants/*': ['constants/*'],
+          'hooks/*': ['hooks/*'],
+          'layout/*': ['layout/*'],
+          'screen/*': ['screen/*'],
+          'types/*': ['types/*'],
+          'utils/*': ['utils/*'],
+        },
+      },
+    ],
+  ],
 };
