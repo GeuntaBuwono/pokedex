3f8394945777d84d7e7df4b07e22165c55e6be6f
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2023-01-23 11:31:05 +0700

    feat(component): label add font-family

---
+++ README.md
@@ -0,0 +1,5 @@
+# How To
+
+## Link React Native Assets
+- add to `<root>/assets`
+- run `npx react-native-asset`
--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -21,6 +21,7 @@ const fontsizeMapper: Record<LabelSize, string> = {
 };
 
 const LabelStyled = styled(Text)<Props>`
+  font-family: 'Poppins-Regular';
   font-size: ${props => fontsizeMapper[props.$size || 'md']};
   color: ${props => props.$color || props.theme.colors.netural[500]};
   text-transform: ${props => props.$textTransform || 'none'};
